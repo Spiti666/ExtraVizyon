afa2b5e8637e82354ed2c87afa0b5b95eede5dde
Spiti666 <106924563+Spiti666@users.noreply.github.com>
2024-10-17 22:29:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 function switchLanguage(language) {
     const englishTexts = document.querySelectorAll('.lang.en');
     const turkishTexts = document.querySelectorAll('.lang.tr');
+    const langEn = document.getElementById('lang-en');
+    const langTr = document.getElementById('lang-tr');
 
     if (language === 'en') {
         englishTexts.forEach(el => el.style.display = 'block');
         turkishTexts.forEach(el => el.style.display = 'none');
-        document.getElementById('lang-en').classList.add('active');
-        document.getElementById('lang-tr').classList.remove('active');
+        langEn.classList.add('active');
+        langTr.classList.remove('active');
     } else if (language === 'tr') {
         englishTexts.forEach(el => el.style.display = 'none');
         turkishTexts.forEach(el => el.style.display = 'block');
-        document.getElementById('lang-en').classList.remove('active');
-        document.getElementById('lang-tr').classList.add('active');
+        langEn.classList.remove('active');
+        langTr.classList.add('active');
     }
 }
 
